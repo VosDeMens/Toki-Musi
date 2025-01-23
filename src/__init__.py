9b50a7652d6fe66c15ea826f46e477d7e17686bd
VosDeMens <vosdemens@gmail.com>
2025-01-23 15:33:19 +0100

    bug fix

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,8 @@
 # from . import clashes_checking as clashes_checking
+# from . import audio_io as audio_io
+
 from . import file_management as file_management
 from . import my_types as my_types
 from . import wave_generation as wave_generation
 from . import util as util
 from . import util_streamlit as util_streamlit
-from . import audio_io as audio_io
