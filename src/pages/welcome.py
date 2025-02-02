66184d04f27b2e494fe7de83bd3046e32e9d9c43
VosDeMens <vosdemens@gmail.com>
2025-02-02 20:03:29 +0100

    minor fix

--- src/pages/welcome.py
+++ src/pages/welcome.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 from src.file_management import WELCOME_TEXT_FILE, load_markdown_from_file
 
-HOME_TEXT = load_markdown_from_file(WELCOME_TEXT_FILE)
+WELCOME_TEXT = load_markdown_from_file(WELCOME_TEXT_FILE)
 
-st.write(HOME_TEXT, unsafe_allow_html=True)  # type: ignore
+st.write(WELCOME_TEXT, unsafe_allow_html=True)  # type: ignore
